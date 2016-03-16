b96eb56cbffa2d677b66227b9941eff736c19163
JC <jc@jcmbp.local>
2016-03-16 16:15:28 -0700

    fixed gamesettings

--- constants/gameSettings.js
+++ constants/gameSettings.js
@@ -1,6 +1,6 @@
 export const startingPoints = 1000;
 export const challengeTimer = 1200; // in seconds
-export const challengeCost = 200;
+export const challengeCost = 100;
 export const teams = [
   'Alpha', 'Bravo', 'Gamma', 'Delta', 'Omega'
 ];
