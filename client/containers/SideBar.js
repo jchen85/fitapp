162fbbe37954f92ac6276dc74b5778a945269f00
JC <jc@jcmbp.local>
2016-03-15 19:11:28 -0700

    style sidebar

--- client/containers/SideBar.js
+++ client/containers/SideBar.js
@@ -9,18 +9,38 @@ export default class SideBar extends Component {
 
     return (
       <div className="sidebar">
-        <div className="sidebar__image">
-          <img src={user.avatar} />
+        <div className="sidebar__user__container">
+          <div className="sidebar__image">
+            <img src={user.avatar} />
+          </div>
+          <div className="sidebar__name">
+            {user.name}, {user.age}
+          </div>
+          <div className="sidebar__team__info">
+            Team: {user.team}
+          </div>
+          Inventory
+          <div className="sidebar__carrots">
+            {user.points} <div className="carrot" />
+          </div>
         </div>
-        {user.name}, {user.age}
-        <div className="sidebar__team__info">
-          Team: {user.team}
+        <div className="sidebar__nav">
+          <div className="sidebar__link">
+            <Link to="/">
+              <img src="../static/icons/clock.png" />Challenges
+            </Link>
+          </div>
+          <div className="sidebar__link">
+            <Link to="/store">
+              <img src="../static/icons/cart.png" />Store
+            </Link>
+          </div>
+          <div className="sidebar__link">
+            <Link to="/leaderboard">
+              <img src="../static/icons/trophy.png" />Leaderboard
+            </Link>
+          </div>
         </div>
-        <ul role="nav">
-          <li><Link to="/">Challenges</Link></li>
-          <li><Link to="/store">Store</Link></li>
-          <li><Link to="/leaderboard">Leaderboard</Link></li>
-        </ul>
       </div>
     );
   }
