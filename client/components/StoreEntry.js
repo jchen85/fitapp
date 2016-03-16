a7289b6c65d332aba0f13a92b83866036a349611
JC <jc@jcmbp.local>
2016-03-15 17:55:02 -0700

    revised store styles

--- client/components/StoreEntry.js
+++ client/components/StoreEntry.js
@@ -9,6 +9,12 @@ export default class StoreEntry extends Component {
     return (
       <div className="store__entry">
         <Card type={type} />
+        <div className="store__cost">
+          -800 <div className="store__carrot" />
+        </div>
+        <button className="store__buy_btn">
+          Buy
+        </button>
       </div>
     );
   }
